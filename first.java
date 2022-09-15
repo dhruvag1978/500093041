8b89aa316adb0c69a64f5f6df4e039553b067126
dhruvag1978 <dhruvagarwal1453@gmail.com>
2022-09-16 02:02:18 +0530

    Updated Java code

--- first.java
+++ first.java
@@ -1,19 +1,50 @@
-import java.util.Scanner;
-public class Test {
-        public static void main(String args[]){
-                Scanner input = null;
-                try {
-                        System.out.println("Please Enter 5 numbers");
-                        input = new Scanner(System.in);
-                        int prod = 1;
-                        for (int i = 0; i < 5; i += 1){
-                                prod *= input.nextInt();
-                        }
-                        System.out.println("The product of numbers is: " + prod );
-                } catch (Exception e) {
-                        e.printStackTrace();
-                } finally {
-                        input.close();
-                }
-        }
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main (String[] args) {	
+	
+		int n = 4;
+		Triplet arr[] = new Triplet[n];
+
+		arr[0] = new Triplet(1, 2, 3);
+		arr[1] = new Triplet(2, 2, 4);
+		arr[2] = new Triplet(5, 6, 1);
+		arr[3] = new Triplet(10, 2, 10);
+
+		// Sorting the array
+		Arrays.sort(arr);
+		
+		// printing the
+		// Triplet array
+		print(arr);		
+	}
+
+	public static void print(Triplet[] arr){
+		for(int i = 0;i < arr.length;i++){
+			System.out.println(arr[i]);
+		}
+	}
+}
+
+class Triplet implements Comparable<Triplet> {
+	int x;
+	int y;
+	int z;
+
+	public Triplet(int x,int y,int z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public String toString() {
+		return "(" + x + "," + y + "," + z + ")";
+	}
+	
+	// Overriden method to compare
+	// values of the last element.
+	public int compareTo(Triplet a){
+		return this.z - a.z;
+	}
 }
